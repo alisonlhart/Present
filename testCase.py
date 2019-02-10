3e51ddd4bd27512aca25a25813ed750b60f6ac84
Jeremy Eudy <jeremyeudy@gmail.com>
2019-02-10 14:49:43 -0500

    Finished API and test case

--- testCase.py
+++ testCase.py
@@ -1,4 +1,6 @@
 import requests
 
-r = requests.post('http://34.73.48.217:5000', data = {'data' : 'testdata123', 'fname' : 'Jeremy', 'lname' : 'Eudy'})
-print(r)
+#r = requests.post('http://34.73.48.217:80', data = {'data' : 'testdata123', 'fname' : 'Jeremy', 'lname' : 'Eudy'})
+#print(r)
+r = requests.get('http://34.73.48.217:80')
+print(r.text)
